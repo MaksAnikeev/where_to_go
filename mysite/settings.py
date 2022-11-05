588a3ac0dd604a52bc7a4ae9b07ea02d66f30a43
MaksAnikeev <anikeev.maks@rambler.ru>
2022-11-05 14:08:22 +0300

    Move data from template to view

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,17 +40,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'places.apps.PlacesConfig'
+    'places.apps.PlacesConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'mysite.urls'
--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from places.models import Place, Image
 import os
 import json
+from pathlib import Path
 
 
 def place_info(number, place):
@@ -17,20 +18,23 @@ def place_info(number, place):
                 "placeId": number,
                 "detailsUrl": f"static/places/{number}.json"
                 },
-          }
+            }
 
 
 def details(place, number):
     place_info = {
-      "title": place.title,
-      "imgs": [image.img.url for image in place.images.all()],
-      "description_short": place.description_short,
-      "description_long": place.description_long,
-      "coordinates": {
-          "lng": place.lng,
-          "lat": place.lat
+        "title": place.title,
+        "imgs": [image.img.url for image in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.lng,
+            "lat": place.lat
       }
-  }
+    }
+    directory_path = os.path.join('static', 'places')
+    Path(directory_path).mkdir(parents=True,
+                               exist_ok=True)
     file_path = os.path.join('static', 'places', f'{number}.json')
     with open(file_path, 'w', encoding='utf8') as json_file:
         json.dump(place_info, json_file, ensure_ascii=False)
@@ -38,11 +42,11 @@ def details(place, number):
 
 
 def index_page(request):
-    places = Place.objects.all()
+    places = Place.objects.prefetch_related('images')
     context = {
-      "places_info": {
-          "type": "FeatureCollection",
-          "features": [place_info(number, place) for number,place in enumerate(places)]}
+        "places_info": {
+            "type": "FeatureCollection",
+            "features": [place_info(number, place) for number,place in enumerate(places)]}
 
     }
     return render(request, 'index.html', context)
