20daa0ac52558304ec1e2cd89e81ee9005d6f963
MaksAnikeev <anikeev.maks@rambler.ru>
2022-11-06 13:25:03 +0300

    Add preview images in inline Place

--- places/admin.py
+++ places/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
 from .models import Place, Image
+from django.utils.html import format_html, mark_safe
 
 
 class ImageInline(admin.TabularInline):
   model = Image
+  fields = ['img', 'preview', 'number']
+  readonly_fields = ['preview']
+
+  def preview(self, obj):
+    return format_html(f'<img src="{obj.img.url}" style="max-height: 100px;">')
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [ImageInline, ]
 
+
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
+    fields = ['place', 'number', 'img', 'preview', ]
     raw_id_fields = ('place', )
+    readonly_fields = ['preview']
 
-
+    def preview(self, obj):
+        return format_html(f'<img src="{obj.img.url}" style="max-height: 100px;">')
