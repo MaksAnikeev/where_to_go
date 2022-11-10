9f12c4c92bf802e5a6b1c98801c9e1e8f8114a87
MaksAnikeev <anikeev.maks@rambler.ru>
2022-11-10 07:00:04 +0300

    Remove adminsortable2

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,11 @@
-from adminsortable2.admin import SortableAdminMixin, SortableStackedInline
+# from adminsortable2.admin import SortableAdminMixin, SortableStackedInline
 from django.contrib import admin
 from django.utils.html import format_html
 
 from .models import Image, Place
 
-
-class ImageTabularInline(SortableStackedInline):
+class ImageInline(admin.TabularInline):
+# class ImageTabularInline(SortableStackedInline):
     model = Image
     fields = ['img', 'preview', 'number']
     ordering = ['number']
@@ -16,8 +16,10 @@ class ImageTabularInline(SortableStackedInline):
 
 
 @admin.register(Place)
-class PlaceAdmin(SortableAdminMixin, admin.ModelAdmin):
-    inlines = [ImageTabularInline, ]
+class PlaceAdmin(admin.ModelAdmin):
+  inlines = [ImageInline, ]
+# class PlaceAdmin(SortableAdminMixin, admin.ModelAdmin):
+#     inlines = [ImageTabularInline, ]
 
 
 @admin.register(Image)
