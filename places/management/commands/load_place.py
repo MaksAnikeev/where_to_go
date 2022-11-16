84f2a80043865908ee62b106f842b85badc6c3a5
MaksAnikeev <anikeev.maks@rambler.ru>
2022-11-16 07:59:35 +0300

    Create function download_images

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand
 from ...models import Image, Place
 import requests
-import os
 from django.core.files.base import ContentFile
 
 
@@ -13,6 +12,18 @@ class Command(BaseCommand):
             'url',
             type=str,
             help=u'url адрес джейсона'
+            )
+
+    def download_images(self, place_params, title, place):
+        imgs = place_params['imgs']
+        for number, img in enumerate(imgs, start=1):
+            response_img = requests.get(img)
+            response_img.raise_for_status()
+            content_file = ContentFile(response_img.content, name=f'{title}{number}.jpg')
+            Image.objects.create(
+                place=place,
+                img=content_file,
+                number=number,
             )
 
     def handle(self, *args, **kwargs):
@@ -34,14 +45,8 @@ class Command(BaseCommand):
             )
 
         if created:
-            imgs = place_params['imgs']
-            for number, img in enumerate(imgs, start=1):
-                response_img = requests.get(img)
-                response_img.raise_for_status()
-                content_file = ContentFile(response_img.content, name=f'{title}{number}.jpg')
-                Image.objects.create(
-                    place=place,
-                    img=content_file,
-                    number=number,
-                    )
+            self.download_images(
+              place_params=place_params,
+              title=title,
+              place=place)
             print(f'Объект {title} с соответствующими картинками создан')
